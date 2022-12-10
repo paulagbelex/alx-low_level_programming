dac20fcafb2449d47b32c84086ba4ce8dc800fb9
paulagbelex <paulagbelex@gmail.com>
2022-12-09 16:55:18 -0800

    num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d\n", num);
+	}
+	return (0);
+}
