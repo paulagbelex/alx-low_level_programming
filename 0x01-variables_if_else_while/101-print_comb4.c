decd732afb68f95f30bc919a2900899c84cb8b65
paulagbelex <paulagbelex@gmail.com>
2022-12-12 04:50:47 -0800

    hmm

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int d1 = 0, d2 = 1, d3 = 2;
+
+	while (d1 <= 9)
+	{
+		putchar(d1 + '0');
+		putchar(',');
+		putchar(' ');
+		putchar(d2 + '0');
+		putchar(d3 + '0');
+		putchar('\n');
+		d1++;
+
+		if (d1 == d2)
+			d2++;
+		if (d1 == d3 || d2 == d3)
+			d3++;
+	}
+	return (0);
+}
+
