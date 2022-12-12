9d8a4799c803d8efb7eba4869e7cc793a4d7342e
paulagbelex <paulagbelex@gmail.com>
2022-12-12 03:40:51 -0800

    spaces nd commas between numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char c = 0;
+
+	while (c < 10)
+	{
+		putchar(c + '0');
+		if (c < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
