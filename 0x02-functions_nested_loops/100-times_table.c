bb4430533def72ff78a6cea6c19412c00239432b
paulagbelex <paulagbelex@gmail.com>
2022-12-14 06:39:02 -0800

    print time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,25 +1,50 @@
 #include "main.h"
 /**
- * print_times_table - prints the time table but ignores 15 and 0
- *
- * @n: prints the time table
- *
+ * print_times_table - prints time table with provided number
+ * @n: the provided value by the user
+ * Return: Always 0 (Success)
  */
+
 void print_times_table(int n)
 {
-	int j, i;
+	int row;
+	int column;
+	int product;
 
-	if (n <= 15 && n >= 0)
+	if (n < 0 || n >= 15)
+		return;
+	for (row = 0; row <= n; row++)
+
 	{
-		for (i = 0; i <= n; i++)
+		for (column = 0; column <= n; column++)
 		{
-			for (j = 0; j <= n; j++)
+			product = (row * column);
+			if (column == 0)
+				_putchar('0' + product);
+			else
 			{
-				_putchar((i * j / 10) + '0');
-				_putchar((i * j % 10) + '0');
+				_putchar(',');
 				_putchar(' ');
+				if (product <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + product);
+				}
+				else if (product > 9 && product < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + (product / 10));
+					_putchar('0' + (product % 10));
+				}
+				else if (product >= 100)
+				{
+					_putchar('0' + (product / 100));
+					_putchar('0' + ((product / 10) % 10));
+					_putchar('0' + (product % 10));
+				}
 			}
-		_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
