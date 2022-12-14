599b5c678ac293426dc507d0fd0058ca19478543
paulagbelex <paulagbelex@gmail.com>
2022-12-14 07:00:04 -0800

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,23 +6,27 @@
  */
 int main(void)
 {
-	int i;
-	int fibonacci[50];
+	long int i, j, k, l;
 
-	fibonacci[0] = 1;
-	fibonacci[1] = 2;
+	j = 1;
+	k = 2;
 
-	for (i = 2; i < 50; i++)
+	for (i = 1; i <= 50; ++i)
 	{
-		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-	}
+		if (j != 20365011074)
+		{
+			printf("%ld, ", j);
+		}
 
-	for (i = 0; i < 50; i++)
-	{
-		printf("%d, ", fibonacci[i]);
-	}
+		else
+		{
+			printf("%ld\n", j);
+		}
 
-	printf("\n");
+		l = j + k;
+		j = k;
+		k = l;
+	}
 
 	return (0);
 }
