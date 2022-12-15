3d12a5f3f23d673ba9268c8d38c82c51677e356f
paulagbelex <paulagbelex@gmail.com>
2022-12-15 15:15:58 -0800

    sorted

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,17 @@
 #include "main.h"
+
 /**
- * main - prints numbers
+ * main - tests function that prints if integer is positive or negative
  *
- * Return: result
+ * Return: 0
  */
+
 int main(void)
 {
-	int i = 0;
+	int i;
 
-	positive_or_negative(i);
+	i = 0;
+	positive_or_negative(int i);
+
 	return (0);
 }
