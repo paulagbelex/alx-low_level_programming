b8428b5f0467ef2d844e9c07b28f12fd7cb6fe41
paulagbelex <paulagbelex@gmail.com>
2022-12-15 16:04:50 -0800

    sorted

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,10 +9,10 @@
 void print_number(int n)
 {
 	if (n < 0)
-       	{
-       		_putchar('-');
-       		n = -n;
-       	}
+	{
+		_putchar('-');
+		n = -n;
+	}
 	if (n / 10)
 	{
 		print_number(n / 10);
