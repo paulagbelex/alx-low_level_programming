a3f83490f1e748393665e82df192f6662d45b4ed
paulagbelex <paulagbelex@gmail.com>
2022-12-19 12:50:27 -0800

    print even number

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2 - entry point
+ *
+ * @str: hold the value
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		printf("%c", *(str + i));
+		i += 2;
+	}
+	printf("\n");
+}
