82d6f12ecc48a11ee440b4b844dc764d1b401329
paulagbelex <paulagbelex@gmail.com>
2022-12-19 13:13:38 -0800

    half the data

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include "main.h"
+/**
+ * puts_half - entry point to print half the data
+ */
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int half = len / 2;
+	int i = half;
+
+	if (len % 2 == 1)
+	{
+		half = (len - 1) / 2;
+	}
+
+	while (i < len)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
