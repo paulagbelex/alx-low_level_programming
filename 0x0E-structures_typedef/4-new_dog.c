4e0bda150227003a017aef4f545a031d19de0ecf
paulagbelex <paulagbelex@gmail.com>
2023-01-16 08:02:41 -0800

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,12 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * *new_dog - function name
+ * @name: name of the ddog
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: always 0
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
-	if(new_dog == NULL)
+
+	if (new_dog == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	new_dog->name = (char *)malloc(strlen(name) + 1);
 	new_dog->owner = (char *)malloc(strlen(owner) + 1);
@@ -16,5 +25,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->age = age;
 	strcpy(new_dog->owner, owner);
 
-	return new_dog;
+	return (new_dog);
 }
