90c2407abe128f0716b582780a8e8241456bdc9b
paulagbelex <paulagbelex@gmail.com>
2023-02-09 04:48:08 -0800

    append to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if(text_content)
+	if (text_content)
 	{
 		for (letter = 0; text_content[letter]; letter++)
 		{
